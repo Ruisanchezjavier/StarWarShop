2b033937c616cab5aa4d2c970b90838f7bee6f06
Ruisanchezjavier <160360911+Ruisanchezjavier@users.noreply.github.com>
2024-07-04 03:05:21 +0000

    puted the backgound

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import logoImg from "../../img/logo_swcs.jpg";
+import "../../styles/signup.css";
 
 
 export const Signup = () => {
@@ -17,12 +18,12 @@ export const Signup = () => {
     
     return (
         <>
-
-       <section className="vh-100" style={{ backgroundColor: '#eee' }}>
+       
+       <section className="vh-100 bg-negro" style={{ backgroundColor: '#eeee' }}>
        <div className="container h-100">
     <div className="row d-flex justify-content-center align-items-center h-100">
       <div className="col-lg-12 col-xl-11">
-        <div className="card text-black" style={{ borderRadius: '25px' }}>
+        <div className="card text-black bg-blanco" style={{ borderRadius: '25px' }}>
           <div className="card-body p-md-5">
             <div className="row justify-content-center">
               <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
