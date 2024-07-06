d440e1b3931fc56987930bb2fdeaf8a341fe870c
AliRahimi123456 <162766248+AliRahimi123456@users.noreply.github.com>
2024-07-06 00:55:01 +0000

    the third upate

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,9 @@ from flask import Flask, request, jsonify, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-# from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -29,8 +31,8 @@ def signin():
     
     user = User.query.filter_by(email=email).first()
     
-    if not user or not user.check_password(password):
-        return jsonify({"msg": "Bad email or password"}), 401
+    # if not user or not user.check_password(password):
+    #     return jsonify({"msg": "Bad email or password"}), 401
 
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token), 200
