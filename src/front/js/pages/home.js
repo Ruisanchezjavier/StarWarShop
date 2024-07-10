c4446e88a431d79b51115ef2975449e65cdabfef
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-07-10 02:19:01 +0000

    loop

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,9 +1,11 @@
 import React, { useContext } from "react"
 import { Context } from "../store/appContext";
 
+import { BackgroundVideo } from '../component/BackgroundVideo';
+
 import "../../styles/home.css";
 
-import { Banner } from '../component/banner';
+// import { Banner } from '../component/banner';
 
 
 export const Home = () => {
@@ -12,9 +14,14 @@ export const Home = () => {
 
 	return (
 
-		<div className="background">
-			
-			<Banner />
+		<div>
+
+			{/* <Banner /> */}
+			<BackgroundVideo />
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <h1>Welcome to My Website</h1>
+        <p>This is an example of a video background in React.</p>
+      </div>
 
 		</div>
 	);
