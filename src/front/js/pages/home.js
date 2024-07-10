cdf4557cc3813be2daef20fda5ed445d4c260f2e
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-07-10 03:27:03 +0000

    d2

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,12 +1,11 @@
 import React, { useContext } from "react"
 import { Context } from "../store/appContext";
 import { StarBackground } from '../component/StarBackground';
-
 import "../../styles/home.css";
 
 // import { Banner } from '../component/banner';
 
- 
+
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
@@ -15,8 +14,8 @@ export const Home = () => {
 			<div>
 			  <StarBackground />
 			  <div style={{ position: 'relative', zIndex: 1 }}>
-				<h1>Welcome to My Website</h1>
-				<p>This is an example of a star background that moves with the mouse in React.</p>
+				<h1>aaaaaaaaaaaaaaaaaa</h1>
+				
 			  </div>
 			</div>
 		  );
