820b1abebec76d34543ebcef31d714918af0ef04
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-07-13 03:40:27 +0000

    1

--- src/front/js/component/Cards/groundCard.js
+++ src/front/js/component/Cards/groundCard.js
@@ -3,15 +3,17 @@ import '../../../styles/groundCard.css';
 
 export const GroundCard = ({ card }) => {
   return ( 
-    <div className="ground-card">
-      <img src={card.image} alt={card.name} />
-      <h2>{card.name}</h2>
-      <p>Rating: {card.rating}</p>
-      <p>Side: {card.side}</p>
-      <p>Type: {card.type}</p>
-      <p>Points: {card.points}</p>
-      <p>Price: ${card.price}</p>
-    </div>
+    <>
+    </>
+    // <div className="ground-card">
+    //   <img src={card.image} alt={card.name} />
+    //   <h2>{card.name}</h2>
+    //   <p>Rating: {card.rating}</p>
+    //   <p>Side: {card.side}</p>
+    //   <p>Type: {card.type}</p>
+    //   <p>Points: {card.points}</p>
+    //   <p>Price: ${card.price}</p>
+    // </div>
   );
 };
 
